4ed8ceab368baf856b95d940853e45c4abad342f
DamianX M Ellwart <damianx.m.ellwart@intel.com>
2018-02-28 23:20:26 +0100

    Textutils formatting

--- src/text/textutils.cpp
+++ src/text/textutils.cpp
@@ -9,69 +9,71 @@
 #include <algorithm>
 
 namespace dp
+
 {
-	namespace textutils
-	{
-		std::string to_upper(std::string text)
-		{
-		    std::transform(text.begin(), text.end(), text.begin(), ::toupper);
-		    return text;
-		}
+    namespace textutils
+    {
+    std::string to_upper(std::string text)
+    {
+        std::transform(text.begin(), text.end(), text.begin(), ::toupper);
+        return text;
+    }
 
-		std::string to_lower(std::string text)
-		{
-		    std::transform(text.begin(), text.end(), text.begin(), ::tolower);
-		    return text;
-		}
+    std::string to_lower(std::string text)
+    {
+        std::transform(text.begin(), text.end(), text.begin(), ::tolower);
+        return text;
+    }
 
-		std::string trim_left(std::string text, std::string chars)
-		{
-		    // TODO: implement
-		    return {};
-		}
+    std::string trim_left(std::string text, std::string chars)
+    {
+        // TODO: implement
+        return {};
+    }
 
-		std::string trim_right(std::string text, std::string chars)
-		{
-		    // TODO: implement
-		    return {};
-		}
+    std::string trim_right(std::string text, std::string chars)
+    {
+        // TODO: implement
+        return {};
+    }
 
-		std::string remove_prefix(std::string text, std::string prefix, int maxHits)
-		{
-		    // TODO: implement
-		    return {};
-		}
+    std::string remove_prefix(std::string text, std::string prefix, int maxHits)
+    {
+        // TODO: implement
+        return {};
+    }
 
-		std::string remove_suffix(std::string text, std::string suffix, int maxHits)
-		{
-		    // TODO: implement
-		    return {};
-		}
+    std::string remove_suffix(std::string text, std::string suffix, int maxHits)
+    {
+        // TODO: implement
+        return {};
+    }
 
 
-		std::string remove_whitespaces(std::string text)
-		{
-			text.erase(
-				std::remove_if(text.begin(), text.end(), [](const auto& letter){
-					return std::isspace(letter);
-				}),
-				text.end()
-			);
+    std::string remove_whitespaces(std::string text)
+    {
+        text.erase(
+            std::remove_if(text.begin(), text.end(), [](const auto& letter)
+        {
+            return std::isspace(letter);
+        }),
+        text.end()
+        );
 
-			return text;
-		}
+        return text;
+    }
 
-		std::string replace(std::string text, std::string oldSeq, std::string newSeq)
-		{
-		    // TODO: implement
-		    return {};
-		}
+    std::string replace(std::string text, std::string oldSeq, std::string newSeq)
+    {
+        // TODO: implement
+        return {};
+    }
 
-		StringArray split(std::string text, std::string delim)
-		{
-		    return {};
-		}
+    StringArray split(std::string text, std::string delim)
+    {
+        return {};
+    }
 
-		std::string join(std::string delim);
-	}
+    std::string join(std::string delim);
+    }
 }
