6fca104e51dd8767be8210fc4d4b974980ae36e7
Damian Ellwart <damianx.m.ellwart@intel.com>
2018-06-07 00:17:51 +0200

    Add missing files

---
+++ code/core/optional.h
@@ -0,0 +1,18 @@
+/*
+ * optional.h
+ *
+ *  Created on: Jun 05, 2018
+ *      Author: Dusteh
+ */
+
+#pragma once
+
+namespace bricks
+{
+    namespace core
+    {
+        class Optional
+        {
+        };
+    }
+}
