1d375e281c7b7e8b3d15fba9b4c9f3f1aa9aa1b2
Damian Ellwart <damianx.m.ellwart@intel.com>
2018-06-07 00:12:11 +0200

    Reorganize sources

---
+++ unit_tests/src/test_optional.cpp
@@ -0,0 +1,21 @@
+/*
+ * test_optional.cpp
+ *
+ *  Created on: Jun 05, 2018
+ *      Author: Dusteh
+ */
+
+#include <gtest/gtest.h>
+#include <core/optional.h>
+
+using bricks::core::Optional;
+
+TEST(OptionalTest, OptionalPositive)
+{
+    EXPECT_EQ(true, true);
+}
+
+TEST(OptionalTest, OptionalNegative)
+{
+    EXPECT_EQ(true, true);
+}
