f91b85f732304af008285df8edd56af2b5f6691d
Damian Ellwart <damianx.m.ellwart@intel.com>
2018-12-18 22:28:29 +0100

    WIP

--- code/core/timing.h
+++ code/core/timing.h
@@ -11,41 +11,78 @@
 #include "optional.h"
 
 namespace bricks {
-    namespace core {
-        class TimeMeasure {
-        public:
+    namespace bench {
+        namespace {
             using TimeResult = std::chrono::nanoseconds;
 
-            template<typename T>
-            struct MeasureResult {
-                TimeResult time;
-                Optional<T> result;
-            };
+            template <typename Func, typename ...Args>
+            using FuncReturn = decltype(std::declval<Func>()(std::declval<Args>()...));
+        }
 
-            template<>
-            struct MeasureResult<void> {
-                TimeResult time;
-            };
-
-            template <typename Func, typename ... Args, typename Ret = decltype(std::declval<Func>()(std::declval<Args>()...)), typename E = enable_if_t<!std::is_same<void, Ret>::value>>
-            static MeasureResult<Ret> measure(Func func, Args ...args) {
-                const auto start = std::chrono::steady_clock::now();
-                auto res = func(std::forward<Args>(args)...);
-                const auto end = std::chrono::steady_clock::now();
-
-                return { std::chrono::duration_cast<TimeResult>(end - start), std::forward<Ret>(res) };
-            }
-
-            template <typename Func, typename ... Args, typename Ret = decltype(std::declval<Func>()(std::declval<Args>()...)), typename E = enable_if_t<std::is_same<void, Ret>::value>>
-            static MeasureResult<void> measure(Func func, Args ...args) {
-                const auto start = std::chrono::steady_clock::now();
-                func(std::forward<Args>(args)...);
-                const auto end = std::chrono::steady_clock::now();
-
-                return { std::chrono::duration_cast<TimeResult>(end - start) };
-            }
-
-        private: 
+        template<typename T>
+        struct MeasureResult {
+            TimeResult time;
+            core::Optional<T> result;
         };
+
+        template<>
+        struct MeasureResult<void> {
+            TimeResult time;
+        };
+
+        template <typename Func, typename ...Args, typename Ret = FuncReturn<Func, Args...>, typename E = enable_if_t<!std::is_void<Ret>::value>>
+        static MeasureResult<Ret> func_bench(Func&& func, Args&& ...args) {
+            const auto start = std::chrono::steady_clock::now();
+            auto res = func(std::forward<Args>(args)...);
+            const auto end = std::chrono::steady_clock::now();
+
+            return{ std::chrono::duration_cast<TimeResult>(end - start), std::forward<Ret>(res) };
+        }
+
+        template <typename Func, typename ...Args, typename Ret = FuncReturn<Func, Args...>, typename E = enable_if_t<std::is_void<Ret>::value>>
+        static MeasureResult<void> func_bench(Func&& func, Args&& ...args) {
+            const auto start = std::chrono::steady_clock::now();
+            func(std::forward<Args>(args)...);
+            const auto end = std::chrono::steady_clock::now();
+
+            return{ std::chrono::duration_cast<TimeResult>(end - start) };
+        }
+
+        template <typename Func/*, typename ...Args*/>
+        class FuncBench {
+        public:
+            using Ret = FuncReturn<Func/*, Args...*/>;
+
+            template <typename ...Args>
+            MeasureResult<Ret> run(Args&& ...args) {
+                results_.push_back(func_bench(fut_, std::forward<Args>(args)...));
+                return results_.back();
+            }
+
+            template <typename ...Args>
+            void run_multiple(const size_t count, Args&& ...args) {
+                results_.reserve(count);
+                for (auto i = 0u; i < count; ++i)
+                    results_.push_back(func_bench(fut_, std::forward<Args>(args)...));
+            }
+
+            size_t size() const { return results_.size(); }
+            
+            FuncBench(Func&& func) : fut_{ std::forward<Func>(func) } {}
+
+        private:
+            Func fut_;
+            std::vector<MeasureResult<Ret>> results_;
+        };
+
+        template <typename Func>
+        static FuncBench<Func> make_func_bench(Func&& func) {
+            return FuncBench<Func>(std::forward<Func>(func));
+        }
+
+        template <typename T, typename Func>
+        static FuncBench<Func> make_func_bench(T&& object, Func&& func) {
+            return FuncBench<Func>([]() {std::forward<Func>(func); });
+        }
     }
 }
--- unit_tests/src/test_timing.cpp
+++ unit_tests/src/test_timing.cpp
@@ -10,34 +10,79 @@
 #include <vector>
 #include <core/timing.h>
 
-using bricks::core::TimeMeasure;
+using bricks::bench::FuncBench;
+using bricks::bench::func_bench;
+using bricks::bench::make_func_bench;
 
-void task_empty() {}
-int task_sum(const std::vector<int>& data) {
-    return std::accumulate(data.begin(), data.end(), 0);
-}
-double task_avg(const std::vector<int>& data, int init) {
-    return std::accumulate(data.begin(), data.end(), init) / data.size();
+namespace {
+
+    void task_empty() {}
+    int task_sum(const std::vector<int>& data) {
+        return std::accumulate(data.begin(), data.end(), 0);
+    }
+    double task_avg(const std::vector<int>& data, int init) {
+        return std::accumulate(data.begin(), data.end(), init) / data.size();
+    }
+
+    int task_const() { return 5; }
+
+    struct Members {
+        int mul(int value) const noexcept { return value * data; }
+    private:
+        int data = 3;
+    };
 }
 
 TEST(TimeMeasureTest, TimeMeasureSimpleStatic)
 {
-    auto meas_1 = TimeMeasure::measure(task_empty);
+    auto meas_1 = func_bench(task_empty);
 
     auto data = std::vector<int>{ 1, 2, 3, 4};
-    auto meas_2 = TimeMeasure::measure([&data]() { return task_sum(data); });
+    auto meas_2 = func_bench([&data]() { return task_sum(data); });
 
     EXPECT_TRUE(meas_1.time.count() > 0);
     EXPECT_TRUE(meas_2.time.count() > 0);
     EXPECT_EQ(meas_2.result.get(), 10);
 }
 
-TEST(TimeMeasureTest, TimeMeatureCallWithVarArgs)
+TEST(TimeMeasureTest, TimeMeasureCallWithVarArgs)
 {
     auto data = std::vector<int>{ 1, 2, 3 };
-    auto meas_1 = TimeMeasure::measure(&task_sum, data);
-    auto meas_2 = TimeMeasure::measure(&task_avg, data, 6);
+    auto meas_1 = func_bench(&task_sum, data);
+    auto meas_2 = func_bench(&task_avg, data, 6);
 
     EXPECT_EQ(meas_1.result.get(), 6);
     EXPECT_EQ(meas_2.result.get(), 4);
+}
+
+TEST(TimeMeasureTest, TimeMeasureViaClassSingleRun)
+{
+    auto meas = make_func_bench(&task_empty);
+    meas.run();
+    meas.run();
+    meas.run();
+    meas.run();
+    EXPECT_EQ(meas.size(), 4);
+}
+
+TEST(TimeMeasureTest, TimeMeasureViaClassMultiRun)
+{
+    auto meas = make_func_bench(&task_const);
+    meas.run_multiple(20);
+    EXPECT_EQ(meas.size(), 20);
+}
+
+TEST(TimeMeasureTest, TimeMeasureClassMember)
+{
+    Members memb{};
+    auto ooo = &Members::mul;
+    auto constexpr ccc = std::is_member_function_pointer<decltype(ooo)>::value;
+    ((memb).*(ooo))(3);
+
+    auto mmm = std::mem_fn(&Members::mul);
+    mmm(&memb, 1);
+    
+    auto meas = make_func_bench(&memb, &Members::mul);
+    /*auto res = meas.run(10);
+    EXPECT_EQ(res.result, 30);*/
 }
